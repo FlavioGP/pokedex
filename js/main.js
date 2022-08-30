cd7b6629125a1420ed94a93acd8b9e44672c86ce
FlavioGP <flavio.gpinheiro@hotmail.com>
2022-08-30 11:53:27 -0300

    adcionando função de visualização de pokemons shiny

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 const pokemonImage = document.querySelector('.pokemon_image')
 const pokemonName = document.querySelector('.pokemon_name')
 const pokemonNumber = document.querySelector('.pokemon_number')
-
+const pokemonMode = document.querySelector('.pokemon-mode');
 const form = document.querySelector('.form');
 const input = document.querySelector('.search');
 
 let pokemonId = 1;
+
 
 const fetchPokemon = async (pokemon) => {
     const apiResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
@@ -15,7 +16,7 @@ const fetchPokemon = async (pokemon) => {
 }
 
 // gifs funcionando até 649
-const renderPokemon = async (pokemon) => {
+const renderPokemon = async (pokemon, shiny) => {
     const data =  await fetchPokemon(pokemon);
     if(!data){ 
         input.value = '';
@@ -24,7 +25,12 @@ const renderPokemon = async (pokemon) => {
     }
     const name = data.name;
     const id = data.id;
-    const img = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    let img;
+    if(!shiny){
+        img = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    }else{
+        img = data['sprites']['versions']['generation-v']['black-white']['animated']['front_shiny'];
+    }
     pokemonImage.src = img;
     pokemonName.innerText = name;
     pokemonNumber.innerText = id;
@@ -38,15 +44,27 @@ form.addEventListener('submit',async (e) =>{
     input.value = '';
 });
 
+
+let shiny;
 document.addEventListener('click', async (e) =>{
     const el = e.target;
-    
     if(el.classList.contains('btn-prev')){ 
         if(pokemonId <= 1) return;   
-        await renderPokemon(pokemonId -= 1);
+        await renderPokemon(pokemonId -= 1, shiny);
     }
-    if(el.classList.contains('btn-next')) await renderPokemon(pokemonId += 1);
+    if(el.classList.contains('btn-next')) await renderPokemon(pokemonId += 1, shiny);
 
+    if(el.classList.contains('shiny-image')){
+        if(el.id == 'normal' ){
+            await renderPokemon(pokemonId, true)
+            el.id = 'shiny'
+            return shiny = true;
+        }else{
+            await renderPokemon(pokemonId, false)
+            el.id = 'normal'
+            return shiny = false;
+        }
+    }
 });
 
 renderPokemon(pokemonId);
